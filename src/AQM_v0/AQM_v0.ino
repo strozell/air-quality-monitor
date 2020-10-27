e24eac039f121d874bda67d3f58e31cd04515e70
strozell <strozell@gmail.com>
2020-10-26 18:12:35 -0700

    Example code - DOES NOT DRIVE HEATER PROPERLY

--- src/AQM_v0/AQM_v0.ino
+++ src/AQM_v0/AQM_v0.ino
@@ -1,37 +1,29 @@
-/*
-  Blink
+const int AOUTpin=0;//the AOUT pin of the CO sensor goes into analog pin A0 of the arduino
+const int DOUTpin=8;//the DOUT pin of the CO sensor goes into digital pin D8 of the arduino
+const int ledPin=13;//the anode of the LED connects to digital pin D13 of the arduino
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+int limit;
+int value;
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+Serial.begin(115200);//sets the baud rate
+pinMode(DOUTpin, INPUT);//sets the pin as an input to the arduino
+pinMode(ledPin, OUTPUT);//sets the pin as an output of the arduino
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+void loop()
+{
+value= analogRead(AOUTpin);//reads the analaog value from the CO sensor's AOUT pin
+limit= digitalRead(DOUTpin);//reads the digital value from the CO sensor's DOUT pin
+Serial.print("CO value: ");
+Serial.println(value);//prints the CO value
+Serial.print("Limit: ");
+Serial.print(limit);//prints the limit reached as either LOW or HIGH (above or underneath)
+delay(100);
+if (limit == HIGH){
+digitalWrite(ledPin, HIGH);//if limit has been reached, LED turns on as status indicator
+}
+else{
+digitalWrite(ledPin, LOW);//if threshold not reached, LED remains off
+}
 }
